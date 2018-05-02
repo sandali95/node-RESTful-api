63bf2fa12393d31acef848c55f5779c15b7cb368
sandali95 <wathmali123@gmail.com>
2018-05-02 08:30:37 +0530

    user login and jwt signing

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,10 +2,11 @@ const express = require('express');
 const User = require('../models/user');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
-router.post('.signup',(req,res)=>{
+router.post('/signup',(req,res)=>{
     
     User.findEmail(req,body.email,(err,data)=>{
         if(data.length >=1){
@@ -43,6 +44,42 @@ router.post('.signup',(req,res)=>{
             });
         }
     });
+});
+
+router.post('./login',(req,res)=>{
+    User.findEmail(req.body.email,(err,user)=>{
+        if(user.length<1){
+            return res.status(402).json({
+                message : "No user exits for the Email"
+            });
+        }else{
+            bcrypt.compare(req.body.password , user[0].password,(err,result)=>{
+                if(err) throw err ;
+
+                jwt.sign({
+                    email : user[0].email,
+                    userId : user[0]._id
+                },
+                'secret',
+                {
+                    expiresIn : '1h'
+                }
+                );
+                if(result){
+                    return res.status(200).json({
+                        message:'Authentication Successful',
+                        token : token
+                    });
+                }else{
+                    return res.status(401).json({
+                        message:'authentication unsuccessful'
+                    });
+                }
+            });
+        }
+    });
+
+
 });
 
 
